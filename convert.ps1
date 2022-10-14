7a97f2d06c66fd840cd4f2ff5e6614b7a2a5ffe4
ANTOND <71350868+antond15@users.noreply.github.com>
2022-10-15 00:10:11 +0200

    feat(convert): Use drag and drop for font path

--- convert.ps1
+++ convert.ps1
@@ -1,9 +1,11 @@
-$fileName = Read-Host 'Enter the file name (with extension)'
-$fontName = Read-Host 'Enter the desired font name'
+# Get user inputs
+$filePath = Read-Host "Drag and drop the font file here (and confirm with enter)"
+$fontName = Read-Host "Enter the desired font name (leave blank for default)"
 
-# Trim spaces
-$trimFileName = $fileName -replace '\s', ''
-Rename-Item -Path $fileName -NewName $trimFileName
+$filePath = $filePath -replace "\\", "/"
+if(-not($fontName)) {
+	$fontName = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
+}
 
 # Load XML
 Set-Location "./tools"
@@ -12,7 +14,7 @@ $inputXml = [xml](Get-Content "input.xml")
 # Edit font attributes
 $inputXml.movie.frame.library.font.SetAttribute("id", $fontName)
 $inputXml.movie.frame.library.font.SetAttribute("name", $fontName)
-$inputXml.movie.frame.library.font.SetAttribute("import", "../$trimFileName")
+$inputXml.movie.frame.library.font.SetAttribute("import", $filePath)
 
 $inputXml.Save("./tools/input.xml")
 
@@ -20,6 +22,7 @@ $inputXml.Save("./tools/input.xml")
 ./swfmill.exe simple input.xml output.swf
 ./gfxexport.exe output.swf | Out-Null
 
+# Clean up and move the result
 Remove-Item "output.swf"
-Move-Item -Path "output.gfx" -Destination "..\output.gfx"
+Move-Item -Path "output.gfx" -Destination "../output.gfx"
 Rename-Item -Path "../output.gfx" -NewName "$fontName.gfx"
